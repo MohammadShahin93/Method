3efbbe6d9fe41eaf94846d0a4759d86cd3d5181c
MohammadShahin23 <M.G.Shah93@gmail.com>
2018-01-13 19:06:28 -0500

    after making changes to carMain class

--- IdeaProjects/string_homework_2017/src/carSimulator/carMain.java
+++ IdeaProjects/string_homework_2017/src/carSimulator/carMain.java
@@ -22,14 +22,14 @@ public class carMain {
         // displays the car model while running in a loop for 5 times and increasing the value of the speed by +10
         System.out.println("\nSeat belt on please. Accelerating...!!!");
         for (int perSecond = 1; perSecond <= 5; perSecond++) {
-            car2.accelerate();
-            System.out.println(car2.getModel() + " " + car2.getMake() + "'s speed is " +
-                    car2.getSpeed() + " mph");
-            if (car2.getSpeed() == 10) {
+            car1.accelerate();
+            System.out.println(car1.getModel() + " " + car1.getMake() + "'s speed is " +
+                    car1.getSpeed() + " mph");
+            if (car1.getSpeed() == 10) {
                 System.out.println("You are on 1st gear");
-            } else if (car2.getSpeed() == 30) {
+            } else if (car1.getSpeed() == 30) {
                 System.out.println("Increasing to 2nd gear");
-            } else if (car2.getSpeed() == 40) {
+            } else if (car1.getSpeed() == 40) {
                 System.out.println("Increasing to 3rd gear");
             }
         }
@@ -37,15 +37,15 @@ public class carMain {
         // displays the car model while running in a loop for 5 times and increasing the value of the speed by +10
         System.out.println("\nBraking...");
         for (int perSecond = 1; perSecond <= 5; perSecond++) {
-            car2.brake();
-            System.out.println(car2.getModel() + " " + car2.getMake() + "'s speed is " +
-                    car2.getSpeed() + " mph");
+            car1.brake();
+            System.out.println(car1.getModel() + " " + car1.getMake() + "'s speed is " +
+                    car1.getSpeed() + " mph");
 
-            if (car2.getSpeed() == 10) {
+            if (car1.getSpeed() == 10) {
                 System.out.println("Decreasing to 1st gear");
-            } else if (car2.getSpeed() == 30) {
+            } else if (car1.getSpeed() == 30) {
                 System.out.println("Decreasing to 2nd gear");
-            } else if (car2.getSpeed() == 40) {
+            } else if (car1.getSpeed() == 40) {
                 System.out.println("You are on 3rd gear");
             }
 
